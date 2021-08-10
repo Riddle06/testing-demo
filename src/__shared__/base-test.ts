dd5598d11e8ae063321eb007453e6547525f9ea7
Rex Li <rex@myre.life>
2021-08-10 18:10:09 +0800

    fix: fix mock function

--- src/__shared__/base-test.ts
+++ src/__shared__/base-test.ts
@@ -3,7 +3,7 @@ import { Connection } from "typeorm";
 import { initializeTransactionalContext } from 'typeorm-transactional-cls-hooked';
 
 jest.mock('typeorm-transactional-cls-hooked', () => ({
-    initializeTransactionalContext: () => { },
+    initializeTransactionalContext: () => jest.fn(),
     Transactional: () => jest.fn(),
     BaseRepository: class { }
 }));
