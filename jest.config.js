aef7e66dac42fc21a97dcfd9a0679e4d7bff153a
Rex Li <rex@myre.life>
2021-08-13 18:28:55 +0800

    fix: add collectCoverageFrom

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   coverageDirectory: "../coverage",
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[t]sx?$',
-  rootDir: "src"
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[t]sx?$",
+  rootDir: "src",
+  collectCoverageFrom: ["**/*.(t|j)s"],
 };
