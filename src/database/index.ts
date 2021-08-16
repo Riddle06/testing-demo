830d93e73184784ad8c71628b4a9a95a254e67a2
Rex Li <rex@myre.life>
2021-08-16 11:42:25 +0800

    feat: add memory cache

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,10 @@
 import { createConnection } from "typeorm";
 import { configuration } from "../configuration";
 import * as path from "path";
-import { ApiUserEntity } from '../entities/api-user.entity';
 
 
 export async function getDBConnection() {
     const { host, port, databaseName, password, user, connectionName, logging } = configuration.db;
-    console.log(`configuration.db`, configuration.db)
     const conn = await createConnection({
         name: connectionName,
         type: "mysql",
--- src/lib/api-user.lib.svc.ts
+++ src/lib/api-user.lib.svc.ts
@@ -4,18 +4,27 @@ import { Inject, Service } from "typedi";
 import { InjectRepository } from "typeorm-typedi-extensions";
 import { APIUserRepository } from '../repositories/api-user.repository';
 import { CreateAPIUserParameter } from '../view-models/api-user.vm';
+import { ApiUserEntity } from '../entities/api-user.entity';
 
 @Service()
 export class APIUserLibSvc {
 
-    
-    @Inject('APIUserRepository') 
+
+    @Inject('APIUserRepository')
     protected readonly apiUserRepository: APIUserRepository
+    protected apiUserEntities: ApiUserEntity[]
 
     async getAPIUsers(): Promise<[APIUser[], Page]> {
+
+        if (this.apiUserEntities) {
+            return [this.apiUserEntities, { dataAmount: this.apiUserEntities.length, pageAmount: 1 }]
+        }
+
         const [entities, count] = await this.apiUserRepository.getAllAPIUsers()
+
+        this.apiUserEntities = entities;
+
         return [entities, { dataAmount: count, pageAmount: 1 }]
-        
     }
 
     async createAPIUser(param: CreateAPIUserParameter): Promise<APIUser> {
